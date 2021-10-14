72ba590142e386afa90a46ea7e1bb64e74d9b146
tymej <tymej12@gmail.com>
2021-10-14 17:09:14 +0200

    Fix samples

--- api/database/data/addresses_samples.php
+++ api/database/data/addresses_samples.php
@@ -3,7 +3,7 @@
 return <<<"SQL"
     INSERT INTO `addresses` (id, street, city) 
         VALUES 
-            ('68839558-7b5c-4ddb-a756-c2aa8094dae4', 'Pawła 9', 'Kraków'),
+            ('68839558-7b5c-4ddb-a756-c2aa8094dae4', 'Pawia 9', 'Kraków'),
             ('35acf9b1-7070-44b6-b79e-5ee2ec298fd0', 'Cyfrowa 8', 'Szczecin')
     ;
 SQL;
